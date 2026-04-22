9ff3fee8fea70182bf446500fa69307144065e3f
BendreShaw <BendreShaw>
2026-04-22 15:35:01 +0200

    Initial React app setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Spotify React App 🎵</h1>
+      <p>Project is running correctly.</p>
+    </div>
+  );
+}
+
+export default App;
